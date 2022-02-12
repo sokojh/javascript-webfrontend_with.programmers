8118a00704651cf15e64d9debcf14a3e7f9db211
jiho kim <sokojiho2@gmail.com>
2022-02-12 15:02:59 +0900

    init

--- gallery/script.js
+++ gallery/script.js
@@ -6,14 +6,40 @@ req.onreadystatechange = function(){
         var date = JSON.parse(this.response);
         for(var i =0 ; i<data.length;i++){
             var div = document.createElement("div");
-            div.setAttribute("class",image);
+            div.setAttribute("class","image");
+           /* div.onclick = function(){
+                if(this.getAttribute("class").indexOf("image-selected")==-1){
+                this.setAttribute("class","image image-selected");
+            }else{
+                this.setAttribute("class","image");
+               이기능을 API로 간단히 표현할수있다. 바로 classList 메소드를 이용하면 됨. 
+            }*/
+            this.classList.toggle("image-selected");
+        
+        }
             var img = document.createElement("img");
             img.src=data[i];
             div.appendChild(img);
             document.body.appendChild(div);
-            
+
         }
     }
 
-}
+
 req.send();
+function selectAll(btn){
+    var images = document.getElementsByClassName("image");
+ for(var i =0;i<images.length;i++){
+     if(btn.value =="Unselect All"){
+     images[i].classList.remove("image-selected");
+ }else{
+     images[i].classList.add("image-selected");
+ }
+}
+if(btn.value == "Unselect All"){
+    btn.value = "Select All";
+    
+}else{
+btn.value = "Unselect All";
+}
+}
